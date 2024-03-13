d3847a99e1ae11cd6f1d2a2adf535fbaa1a7626a
Yudha Dewantara <yudha.dewantaraa@gmail.com>
2024-03-13 22:28:06 +0700

    Portal berita Effect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import NewsPortal from "./NewsPortal";
 import "./styles.css";
 import "./script.js";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // Set state awal jika diperlukan
-    };
-  }
 
-  componentDidMount() {
-    // Dipanggil setelah komponen telah dimasukkan ke dalam DOM
+function App() {
+  useEffect(() => {
+    // Dipanggil setelah komponen pertama kali dimasukkan ke dalam DOM (menggantikan componentDidMount)
     console.log("Component did mount");
-  }
 
-  componentDidUpdate(prevProps, prevState) {
-    // Dipanggil setelah pembaruan komponen
+    return () => {
+      // Dipanggil sebelum komponen dihapus dari DOM (menggantikan componentWillUnmount)
+      console.log("Component will unmount");
+    };
+  }, []); // Gunakan array kosong sebagai dependencies untuk memastikan useEffect hanya dijalankan sekali saat komponen dipasang
+
+  useEffect(() => {
+    // Dipanggil setelah pembaruan komponen (menggantikan componentDidUpdate)
     console.log("Component did update");
-  }
+  });
 
-  componentWillUnmount() {
-    // Dipanggil sebelum komponen dihapus dari DOM
-    console.log("Component will unmount");
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="bg-dark text-white text-center py-4">
-          <h1>Portal Berita</h1>
-        </header>
-        <NewsPortal />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="bg-dark text-white text-center py-4">
+        <h1>Portal Berita</h1>
+      </header>
+      <NewsPortal />
+    </div>
+  );
 }
 
 export default App;
